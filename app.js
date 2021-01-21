d97cba788710a5c9d6f74fcc188944330bafb3fe
Kelvin Bush <kelybush@gmail.com>
2021-01-21 12:45:14 +0300

    Adding ejs layouts

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ let bodyParser = require("body-parser");
 let app = express();
 
 let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use('*/css', express.static('public/css'));
 
 app.get("/", (req, res) => {
     let today = new Date();
@@ -21,13 +23,36 @@ app.get("/", (req, res) => {
     let day = today.toLocaleDateString("en-US", options);
 
 
-    res.render('list', {today: day, newListItems: items});
+    res.render('list', {listTitle: day, newListItems: items});
 });
 
 app.post("/", (req, res) => {
-    items.push(req.body.newItem);
-    res.redirect("/");
+    console.log(req.body);
+    let item = req.body.newItem;
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+
+
 })
+
+app.get("/about", (req, res) => {
+   res.render("about");
+});
+
+app.get("/work", (req, res) => {
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.post("/work", (req, res) => {
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+});
 
 
 app.listen(3000, () => console.log("server started on port 3000"));
