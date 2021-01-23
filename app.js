9c5bf63162cb0951f8bb4d86fb17c8da1c6a2725
Kelvin Bush <kelybush@gmail.com>
2021-01-23 13:19:36 +0300

    add gitignore procfile

--- app.js
+++ app.js
@@ -142,5 +142,9 @@ app.post("/work", (req, res) => {
     res.redirect("/work");
 });
 
+let port = process.env.PORT;
+if(port == null || port === ""){
+    port = 3000;
+}
 
-app.listen(3000, () => console.log("server started on port 3000"));
+app.listen(port, () => console.log("server started Successfully"));
